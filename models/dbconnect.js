dbf00b69a069d0054b17f0a792d73ecd3d19c829
Pankaj <pspankajsingh395@gmail.com>
2019-12-17 20:48:36 +0530

    push

--- models/dbconnect.js
+++ models/dbconnect.js
@@ -8,7 +8,7 @@ var neo4j = require('neo4j-driver');
 var graphenedbURL = process.env.GRAPHENEDB_BOLT_URL;
 var graphenedbUser = process.env.GRAPHENEDB_BOLT_USER;
 var graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD;
-
+console.log("cred",graphenedbURL,graphenedbUser,graphenedbPass)
 var driver = neo4j.driver(graphenedbURL, neo4j.auth.basic(graphenedbUser, graphenedbPass));
 
 // var driver = neo4j.driver(nconf.get('neo4j-local'),neo4j.auth.basic(nconf.get('USERNAME'),nconf.get('PASSWORD')));
